af77b7500268cea2cdd328a6ce78e80c88290c18
Jdexurbano <dexterurbano25@gmail.com>
2025-03-14 10:59:41 +0800

    edit the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import MainLayout from "./shared/components/layouts/MainLayout.tsx";
-import LoginPage from "./pages/Login.tsx";
-import Register from "./pages/Register.tsx";
-import StudentInformation from "./pages/studentInformation.tsx"; // ✅ Import Student Information Page
-
-// Define Routes
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-  },
-  {
-    path: "/auth",
-    element: <LoginPage />,
-    children: [
-      { path: "/auth/register", element: <Register /> },
-      {
-        path: "/auth/register",
-        element: (
-          <div className="bg-red-500 flex h-28 flex-col justify-content-center w-[30%] align-items-center border-3 rounded-lg">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button>Login</button>
-          </div>
-        ),
-      },
-    ],
-  },
-  {
-    path: "/student-information", // ✅ Add Student Information Route
-    element: <StudentInformation />,
-  },
-]);
+import App from "./App";
+import { BrowserRouter } from "react-router";
 
 // Render Application
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={routes} />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
